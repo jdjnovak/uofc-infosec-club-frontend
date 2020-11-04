3f7ef761bb8cac659f8ca7203ed4e5e97bc617ee
Joshua Cordeiro-Zebkowitz <zebkowitz@gmail.com>
2020-11-04 02:05:30 -0700

    Started Resources Update
    Added search bar, it's not connected to anything yet.
    Started creating a category component so that each resource is organized by category.

---
+++ src/components/ResourceCategory.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="py-8 w-full">
+    <div class="shadow overflow-x-auto scrolling-touch rounded border-b border-gray-200">
+      <table id="resources" class="min-w-full">
+        <thead class="bg-primary text-white">
+          <tr>
+            <th class="hidden md:table-cell w-1/6 text-center py-3 px-4 uppercase font-semibold text-sm">Category</th>
+          </tr>
+        </thead>
+        <tbody class="bg-gray-100 text-gray-700 font-display">
+          <tr class="hidden md:table-cell w-1/6 py-3 px-4">
+            <td class="float-left">Title</td>
+            <td class="float-right">Description</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+<style>
+#resources thead {
+  /* background-color: #12418f !important; */
+  color: #FFF !important;
+}
+#resources tbody {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
+<script>
+export default {
+  name: 'ResourceCategory',
+  data () {
+
+  },
+  methods: {
+
+  },
+  computed: {
+  },
+  watch: {
+  }
+}
+</script>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -2,16 +2,35 @@
   <div class="py-8 text-center w-3/4 mx-auto">
     <br>
     <p class="font-display text-4xl">Resources List</p>
-    <ResourcesTableComponent />
+    <div>
+      <input type="text" name ="search" placeholder="Search Resources...">
+    </div>
+    <br>
+    <ResourceCategory />
+    <!--<ResourcesTableComponent />-->
   </div>
 </template>
+<style scoped>
+  input {
+    border-width: 1px;
+    border-color: #a0aec0;
+    color: #000;
+    --placeholder-opacity: 1;
+    float: left;
+    border-radius: 0.375rem;
+    width: 100%;
+    padding: 0.5rem;
+  }
+</style>
 <script>
-import ResourcesTableComponent from '@/components/ResourcesTableComponent'
+// import ResourcesTableComponent from '@/components/ResourcesTableComponent'
+import ResourceCategory from '@/components/ResourceCategory'
 
 export default {
   name: 'Resources',
   components: {
-    ResourcesTableComponent
+    ResourceCategory
+    // ResourcesTableComponent
   }
 }
 </script>
